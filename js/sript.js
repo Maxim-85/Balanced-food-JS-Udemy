d070cdf51ab99b8d38599fce8870d85622e3e08e
Krivoruchko Maxim <23maxikriv07@gmail.com>
2022-10-28 23:12:42 +0500

    creat timer for daeadline

--- js/sript.js
+++ js/sript.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
-// tabs
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
+// Tabs
+    let tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
     function hideTabContent () {
         tabsContent.forEach(item => {
@@ -24,10 +24,10 @@ document.addEventListener('DOMContentLoaded', () => {
     hideTabContent ();
     showTabContent ();
 
-//  вешааем обработчик на родителя
+//  Вешаем обработчик на родителя
     tabsParent.addEventListener('click', (event) => {
         const target = event.target;
-        // проверяем, что событие произошло именно на конкретном элементе с         конкретным классом
+        // Проверяем, что событие произошло именно на конкретном элементе с конкретным классом
         if (target && target.classList.contains('tabheader__item')) {
 
             tabs.forEach((item, i) => {
@@ -39,5 +39,59 @@ document.addEventListener('DOMContentLoaded', () => {
             
         }
     });
+
+    // Timer
+
+    const deadline = '2023-02-01';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num) {
+        if(num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timerInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timerInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 
 });
